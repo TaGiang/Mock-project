2f7334484800152480f919437262fc689a39e60f
hieu nguyen <hieunguyenmanh13.cdc@gmail.com>
2022-08-08 12:22:53 +0700

    create project

---
+++ src/main/java/dao/impl/ExampleDAOImpl.java
@@ -0,0 +1,7 @@
+package dao.impl;
+
+import dao.ExampleDAO;
+
+public class ExampleDAOImpl implements ExampleDAO {
+
+}
---
+++ src/main/java/service/impl/ExampleServiceImpl.java
@@ -0,0 +1,11 @@
+package service.impl;
+
+import dao.ExampleDAO;
+import dao.impl.ExampleDAOImpl;
+import service.ExampleService;
+
+import java.util.List;
+
+public class ExampleServiceImpl implements ExampleService {
+
+}
